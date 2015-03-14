a9e33eb20b47ec099683534cace198efe4188816
Raoul <raoulb@bluewin.ch>
2015-03-14 03:14:18 +0100

    First version of a deployment script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+
+BUILD=./build/
+TARGET=/home/raoul/ENIGMA/homepage_update/enigma/
+
+set -e -o pipefail
+
+# Remove old build
+rm -rf $BUILD
+mkdir $BUILD
+
+# Clone and build
+git clone . $BUILD
+(
+    cd $BUILD;
+    commit=$(git rev-parse HEAD);
+    # TODO: put git id into footer
+    chmod u+x main.lua;
+    lua5.1 main.lua;
+)
+
+# Copy over
+rsync -av --exclude='.git' --exclude='input' $BUILD/* $TARGET
+
+# Commit into cvs
+(
+    cd $TARGET
+    pwd
+    #cvs add ./*
+    #cvs commit -m "Update"
+)
